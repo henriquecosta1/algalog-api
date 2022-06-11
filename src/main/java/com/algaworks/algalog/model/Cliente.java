24cfb8aaae9cf18cd11d7cb684032dfcf6315c67
FABIO HENRIQUE CRISTO COSTA <02141087244@TRT8.net>
2022-06-11 17:34:00 -0300

    temp

--- src/main/java/com/algaworks/algalog/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/model/Cliente.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.validation.groups.Default;
 
 import com.algaworks.algalog.ValidationGroups;
 
--- src/main/java/com/algaworks/algalog/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/model/Entrega.java
@@ -1,7 +1,7 @@
 package com.algaworks.algalog.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.Objects;
 
 import javax.persistence.Embedded;
@@ -47,10 +47,10 @@ public class Entrega {
 	private StatusEntrega status;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataPedido;
+	private OffsetDateTime dataPedido;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 	
 	public Long getId() {
 		return id;
@@ -82,16 +82,16 @@ public class Entrega {
 	public void setStatus(StatusEntrega status) {
 		this.status = status;
 	}
-	public LocalDateTime getDataPedido() {
+	public OffsetDateTime getDataPedido() {
 		return dataPedido;
 	}
-	public void setDataPedido(LocalDateTime dataPedido) {
+	public void setDataPedido(OffsetDateTime dataPedido) {
 		this.dataPedido = dataPedido;
 	}
-	public LocalDateTime getDataFinalizacao() {
+	public OffsetDateTime getDataFinalizacao() {
 		return dataFinalizacao;
 	}
-	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
 	}
 	
