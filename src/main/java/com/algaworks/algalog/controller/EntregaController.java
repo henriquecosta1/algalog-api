04b96a281dcbbc048f72d1c3b52a1d72866415d2
FABIO HENRIQUE CRISTO COSTA <02141087244@TRT8.net>
2022-06-11 18:11:37 -0300

    Adicionando biblioteca ModelMapper no projeto

--- src/main/java/com/algaworks/algalog/controller/EntregaController.java
+++ src/main/java/com/algaworks/algalog/controller/EntregaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algalog.dto.DestinatarioDTO;
 import com.algaworks.algalog.dto.EntregaDTO;
 import com.algaworks.algalog.model.Entrega;
 import com.algaworks.algalog.repository.EntregaRepository;
@@ -30,6 +30,9 @@ public class EntregaController {
 	
 	@Autowired
 	private EntregaRepository entregaRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -46,19 +49,7 @@ public class EntregaController {
 	public ResponseEntity<EntregaDTO> buscar(@PathVariable Long entregaId) {
 		return entregaRepository.findById(entregaId)
 				.map(entrega -> {
-					EntregaDTO entregaDTO = new EntregaDTO();
-					entregaDTO.setId(entrega.getId());
-					entregaDTO.setNomeCliente(entrega.getCliente().getNome());
-					entregaDTO.setDestinatario(new DestinatarioDTO());
-					entregaDTO.getDestinatario().setNome(entrega.getDestinatario().getNome());
-					entregaDTO.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
-					entregaDTO.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
-					entregaDTO.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
-					entregaDTO.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
-					entregaDTO.setTaxa(entrega.getTaxa());
-					entregaDTO.setStatus(entrega.getStatus());
-					entregaDTO.setDataPedido(entrega.getDataPedido());
-					entregaDTO.setDataFinalizacao(entrega.getDataFinalizacao());					
+					EntregaDTO entregaDTO = modelMapper.map(entrega, EntregaDTO.class);
 					return ResponseEntity.ok(entregaDTO);
 				})
 				.orElse(ResponseEntity.notFound().build());
